216b0f34691f292cd3f62806648f448989332127
Sam Parsons <samparsons269@gmail.com>
2019-01-17 22:24:05 -0500

    gitignore

---
+++ ios/RNPDMetronome/PDPatch.h
@@ -0,0 +1,17 @@
+//
+//  PDPatch.h
+//  RNPDMetronome
+//
+//  Created by Sam Parsons on 1/17/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PDPatch : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/RNPDMetronome/PDPatch.m
@@ -0,0 +1,13 @@
+//
+//  PDPatch.m
+//  RNPDMetronome
+//
+//  Created by Sam Parsons on 1/17/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import "PDPatch.h"
+
+@implementation PDPatch
+
+@end
